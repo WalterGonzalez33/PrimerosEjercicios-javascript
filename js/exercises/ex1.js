5e41280b02db1fddc7a81984e03a978da1f9cf9e
Walter Osvaldo Gonzalez <wg904093@gmail.com>
2024-07-01 13:38:24 -0300

    modification the exercises

--- js/exercises/ex1.js
+++ js/exercises/ex1.js
@@ -1,1 +1,9 @@
+// alert('hello world')
+
+const markdownContent = `
+\`\`\`
 alert('hello world')
+`;
+
+document.getElementById("content").innerHTML = marked.parse(markdownContent);
+document.querySelector("code").className += "language-js";
--- js/exercises/ex9.js
+++ js/exercises/ex9.js
@@ -1,39 +1,38 @@
-// Prompt para que el usuario ingrese la frase o palabra a evaluar
-const phrase = prompt("write a phrase: ");
+// // Prompt para que el usuario ingrese la frase o palabra a evaluar
+// const phrase = prompt("write a phrase: ");
 
-// Ejercicio resuelto con los metodos sugeridos en la consigna (.substring y .charAt)
-const vowels = "aeiou";
-let phraseVowels = "";
-for (let i = 0; i < phrase.length; i++) {
-  for (let j = 0; j < vowels.length; j++) {
-    if (phrase.charAt(i) === vowels.charAt(j)) {
-      phraseVowels += phrase.substring(i, i + 1);
-    }
-  }
-}
+// // Ejercicio resuelto con los metodos sugeridos en la consigna (.substring y .charAt)
+// const vowels = "aeiou";
+// let phraseVowels = "";
+// for (let i = 0; i < phrase.length; i++) {
+//   for (let j = 0; j < vowels.length; j++) {
+//     if (phrase.charAt(i) === vowels.charAt(j)) {
+//       phraseVowels += phrase.substring(i, i + 1);
+//     }
+//   }
+// }
 
-// Solución alternativa que quise incluir
-const handlerVowels = (phrase) => {
-  if (typeof phrase != "string") {
-    throw new Error("phrase is not string");
-  }
-  const phraseToLowerCase = phrase.toLowerCase();
-  const vowels = ["a", "e", "i", "o", "u"];
-  let PhraseVowels = [];
-  for (let i = 0; i < phraseToLowerCase.length; i++) {
-    for (let j = 0; j < vowels.length; j++) {
-      if (phraseToLowerCase[i] === vowels[j]) PhraseVowels.push(phrase[i]);
-    }
-  }
-  return PhraseVowels.join("");
-};
+// // Solución alternativa que quise incluir
+// const handlerVowels = (phrase) => {
+//   if (typeof phrase != "string") {
+//     throw new Error("phrase is not string");
+//   }
+//   const phraseToLowerCase = phrase.toLowerCase();
+//   const vowels = ["a", "e", "i", "o", "u"];
+//   let PhraseVowels = [];
+//   for (let i = 0; i < phraseToLowerCase.length; i++) {
+//     for (let j = 0; j < vowels.length; j++) {
+//       if (phraseToLowerCase[i] === vowels[j]) PhraseVowels.push(phrase[i]);
+//     }
+//   }
+//   return PhraseVowels.join("");
+// };
 
-// Muestra resultados por consola
-console.log(phraseVowels);
-console.log(handlerVowels(phrase));
+// // Muestra resultados por consola
+// console.log(phraseVowels);
+// console.log(handlerVowels(phrase));
 
 const markdownContent = `
-#### codigo
 \`\`\`
 // Prompt para que el usuario ingrese la frase o palabra a evaluar
 const phrase = prompt("write a phrase: ");
